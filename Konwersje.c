8a58286211f9e73ecc9167f3945a07994bfa97d5
michal_jez <michaljezz@gmail.com>
2017-12-17 20:31:58 +0100

    Konwersje-update
    Notation of 'for' loops was changed according to convention.
    Some code modification was made to improve functionality.

--- Konwersje.c
+++ Konwersje.c
@@ -4,46 +4,53 @@ enum Result{OK, ERROR};
 
 void UIntToHexStr(unsigned int uiValue, char pcStr[]){
     
-    unsigned char ucCurrentTetrad;
-    char cTetradValue;
+    unsigned int ucCurrentTetrad;
+    int cTetradValue;
     
     pcStr[0]='0';
     pcStr[1]='x';
-    for(ucCurrentTetrad=0;4>ucCurrentTetrad;ucCurrentTetrad++){				// KONWENCJA
-        cTetradValue = ((uiValue >> ((5-ucCurrentTetrad) * 0xF)) & 0xF); 
-        if(cTetradValue < 10){
-            pcStr[ucCurrentTetrad] = '0' + cTetradValue;
-        }
-        else{
-            pcStr[ucCurrentTetrad] = 'A' + cTetradValue - 10;
-        }
+    for(ucCurrentTetrad=0; ucCurrentTetrad<4; ucCurrentTetrad++){
+		cTetradValue=(uiValue >> (4 * (3-ucCurrentTetrad))) & 0xF;
+		if(cTetradValue>10){
+			pcStr[ucCurrentTetrad+2]=cTetradValue-10+'A';
+		}
+		else{
+			pcStr[ucCurrentTetrad+2]=cTetradValue+'0';
+		}
     }
-    pcStr[ucCurrentTetrad]=NULL;
+    pcStr[ucCurrentTetrad+2]=NULL;
+	
 }
 
 enum Result eHexStringToUInt(char pcStr[], unsigned int *puiValue){
     
-    unsigned char ucCurrentChar;
+    unsigned int ucCurrentChar;
     unsigned int uiCharValue;
     
-    if(('0'!=pcStr[0])||('x'!=pcStr[1])){
+    if(('0'!=pcStr[0]) || ('x'!=pcStr[1])){
 		return ERROR;
 	}
-	for(ucCurrentChar=0;!(NULL==pcStr[ucCurrentChar]);ucCurrentChar++){				//KONWENCJA
-		if(pcStr[ucCurrentChar]<'A'){
-			uiCharValue=uiCharValue+((pcStr[ucCurrentChar]-'0')&0xF);
+	for(ucCurrentChar=2; !(NULL==pcStr[ucCurrentChar]); ucCurrentChar++){
+		if((pcStr[ucCurrentChar] <= '9') && (pcStr[ucCurrentChar] >= '0')){
+			uiCharValue=pcStr[ucCurrentChar]-'0';
+			*puiValue=(*puiValue<<4)+uiCharValue;
+		}
+		else if((pcStr[ucCurrentChar] <= 'F') && ( pcStr[ucCurrentChar] >= 'A')){
+			uiCharValue=pcStr[ucCurrentChar]-'A'+10;
+			*puiValue=(*puiValue<<4)+uiCharValue;
+		}
+		else if((pcStr[ucCurrentChar] <= 'f') && ( pcStr[ucCurrentChar] >= 'a')){
+			uiCharValue=pcStr[ucCurrentChar]-'a'+10;
 			*puiValue=(*puiValue<<4)+uiCharValue;
 		}
 		else{
-			uiCharValue=uiCharValue+10+((pcStr[ucCurrentChar]-'A')&0xF);
-			*puiValue=(*puiValue<<4)+uiCharValue;
+			return ERROR;
 		}
 	}
-	if((8>ucCurrentChar)&&(3<ucCurrentChar)){
+	if((7>ucCurrentChar) && (2<ucCurrentChar)){
 		return OK;
 	}
 	else {
-		*puiValue = 0;
 		return ERROR;
 	}
 }
@@ -54,14 +61,4 @@ void AppendUIntToString(unsigned int uiValue, char pcDestinationStr[]){
     
     for(ucStringIndex=0;!(NULL==pcDestinationStr[ucStringIndex]);ucStringIndex++){}
     UIntToHexStr(uiValue,(&pcDestinationStr[ucStringIndex]));
-}
-
-int main(){
-	
-	char znak[] = "0x44";
-	unsigned int liczba;
-	unsigned int * wskaznik = &liczba;
-	
-	eHexStringToUInt(znak,  wskaznik);
-	printf("%02x", liczba);
 }
